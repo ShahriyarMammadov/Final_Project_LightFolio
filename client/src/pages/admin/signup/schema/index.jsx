f940d988867ec454978f6b8310cbc1edbab40227
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-02-10 20:10:40 +0400

    schema

--- client/src/pages/admin/signup/schema/index.jsx
+++ client/src/pages/admin/signup/schema/index.jsx
@@ -4,14 +4,14 @@ const LoginSchema = Yup.object().shape({
   email: Yup.string()
     .email("Please enter a valid email address.")
     .required("Email is required."),
-  fullName: Yup.string()
-    .min(4, "Too Short!")
-    .max(40, "Too Long!")
-    .required("Full Name is required."),
-  companyName: Yup.string()
-    .min(4, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Company Name is required."),
+  // fullName: Yup.string()
+  //   .min(4, "Too Short!")
+  //   .max(40, "Too Long!")
+  //   .required("Full Name is required."),
+  // companyName: Yup.string()
+  //   .min(4, "Too Short!")
+  //   .max(50, "Too Long!")
+  //   .required("Company Name is required."),
   password: Yup.string()
     .min(10, "Too Short!")
     .max(50, "Too Long!")
